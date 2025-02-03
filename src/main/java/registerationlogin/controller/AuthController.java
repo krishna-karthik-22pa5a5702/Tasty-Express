8faa237c71550cfcb78aea9e75f6a31e4332b86e
rohitshimpi737 <rohit.22210261@viit.ac.in>
2025-02-03 15:59:01 +0530

    AuthController Updated

--- src/main/java/registerationlogin/controller/AuthController.java
+++ src/main/java/registerationlogin/controller/AuthController.java
@@ -16,72 +16,100 @@ import java.util.List;
 
 @Controller
 public class AuthController {
-    
+
     private UserService userService;
 
     public AuthController(UserService userService) {
-        this.userService=userService;
+        this.userService = userService;
     }
 
-
-    //handler method to handle home page.
+    // handler method to handle home page.
     // @GetMapping("/")
     // public String home(){
-    //     return "index"; 
+    // return "index";
     // }
 
+    // @GetMapping("/")
+    // public String home() {
+    // return "home_copy";
+    // }
 
-     @GetMapping("/")
-    public String home(){
-        return "home_copy"; 
+    @GetMapping("/")
+    public String home() {
+        return "home-final";
     }
 
+    // handler method to handle user registeration form request.
 
-    //handler method to handle user registeration form request.
+    // @GetMapping("/register")
+    // public String showRegistrationForm(Model model) {
+
+    // UserDto userDto = new UserDto();
+    // model.addAttribute("user", userDto); // model object is used to store data
+    // that is entered from form.
+    // return "register";
+    // }
 
     @GetMapping("/register")
-    public String showRegistrationForm(Model model){
+    public String showRegistrationForm(Model model) {
 
         UserDto userDto = new UserDto();
-        model.addAttribute("user",userDto); //model object is used to store data that is entered from form.
-        return "register";
+        model.addAttribute("user", userDto); // model object is used to store data that is entered from form.
+        return "register_copy";
     }
-    //handler method to handle user registration form submit request.
+    // handler method to handle user registration form submit request.
 
     @PostMapping("/register/save")
-    public String registration(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result,Model model){ //Model attribute is used to extract model object which is a form data.
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result, Model model) { // Model
+                                                                                                                    // attribute
+                                                                                                                    // is
+                                                                                                                    // used
+                                                                                                                    // to
+                                                                                                                    // extract
+                                                                                                                    // model
+                                                                                                                    // object
+                                                                                                                    // which
+                                                                                                                    // is
+                                                                                                                    // a
+                                                                                                                    // form
+                                                                                                                    // data.
 
+        User existingUser = userService.findByEmail(userDto.getEmail()); // checking if entered email already exists or
+                                                                         // not.
 
-        User existingUser = userService.findByEmail(userDto.getEmail()); //checking if entered email already exists or not.
-
-        if(existingUser!=null && existingUser.getEmail()!=null && !existingUser.getEmail().isEmpty()){
-            result.rejectValue("email",null,"there is already an account existed with this email");
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            result.rejectValue("email", null, "there is already an account existed with this email");
         }
 
-        if(result.hasErrors()){
-            model.addAttribute("user",userDto);
+        if (result.hasErrors()) {
+            model.addAttribute("user", userDto);
             return "/register"; // if any form has errors it will be redirected to register page only.
         }
 
         userService.saveUser(userDto);
-        return "redirect:/register?success"; // @Valid from jakarta.validation will enable the validation fields of dto objectsto be enabled.
+        return "redirect:/register?success"; // @Valid from jakarta.validation will enable the validation fields of dto
+                                             // objectsto be enabled.
 
     }
 
-    //handler methods for getting list of users.
+    // handler methods for getting list of users.
     @GetMapping("/admin/dashboard")
-    public String users(Model model){
+    public String users(Model model) {
         List<UserDto> users = userService.findAllUsers();
-        model.addAttribute("users",users);
+        model.addAttribute("users", users);
         return "dashboard";
     }
-    
 
-    //handler methods for handling login request.
+    // handler methods for handling login request.
+
+    // @GetMapping("/login")
+    // public String login(){
+    // return "login";
+    // }
 
     @GetMapping("/login")
-    public String login(){
-        return "login";
+    public String login() {
+        return "login_copy";
     }
 
 }
